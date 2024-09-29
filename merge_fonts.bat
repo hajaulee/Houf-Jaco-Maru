e9e56046ff6327c5f592ac1f489a27abffc9ba46
Le Cong Hau <anhlahau.hl@gmail.com>
2024-09-30 00:15:56 +0900

    Generate jaco font

---
+++ merge_fonts.bat
@@ -0,0 +1,5 @@
+fontforge -lang=ff -script merge_fonts.ff ^
+  .\new_fonts\ttf\HoufJacoMaru-Regular_base.ttf ^
+  .\generated\HoufJacoMaru-Regular.ttf ^
+  1000 ^
+  .\new_fonts\ttf\HoufJacoMaru-Regular.ttf
